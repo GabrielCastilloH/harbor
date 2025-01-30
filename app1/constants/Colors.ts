e74e2ff9ac0b601f7559509e31b5036d8fd47f99
Gabriel <gac232@cornell.edu>
2025-01-29 21:41:37 -0500

    Basic tabbar working

---
+++ app1/constants/Colors.ts
@@ -0,0 +1,11 @@
+export default {
+  primary500: '#800000', // maroon
+  primary400: '#A52A2A',
+  primary300: '#C41E3A',
+  secondary500: '#FFFFFF', // white
+  secondary400: '#F5F5F5',
+  secondary300: '#E8E8E8',
+  accent500: '#D3D3D3',
+  accent400: '#C0C0C0',
+  accent300: '#A9A9A9'
+};
