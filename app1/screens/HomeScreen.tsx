2f1d7713a68a861471c464a6c8dcea89e7315fd6
Gabriel <gac232@cornell.edu>
2025-01-30 14:39:30 -0500

    add logo

--- app1/screens/HomeScreen.tsx
+++ app1/screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Image } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import Colors from '../constants/Colors';
 import { mockProfiles } from '../constants/Data';
@@ -8,6 +8,13 @@ import AnimatedStack from '../components/AnimatedStack';
 export default function HomeScreen() {
   return (
     <GestureHandlerRootView style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Image 
+          source={require('../assets/icon.png')} 
+          style={styles.logo}
+          resizeMode="contain"
+        />
+      </View>
       <View style={styles.cardsContainer}>
         <AnimatedStack
           data={mockProfiles}
@@ -23,6 +30,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.secondary500,
+  },
+  headerContainer: {
+    width: '100%',
+    paddingTop: 70,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: Colors.primary100,
+  },
+  logo: {
+    height: 40,
+    width: 120,
   },
   cardsContainer: {
     flex: 1,
