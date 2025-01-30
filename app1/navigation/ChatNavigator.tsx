44fc0c4f5ed0d02d15f2a0295d5d989d830428fe
Gabriel <gac232@cornell.edu>
2025-01-30 12:36:03 -0500

    put chat loading screen in ChatNavigator, instead of having it load before the app can be shown.

--- app1/navigation/ChatNavigator.tsx
+++ app1/navigation/ChatNavigator.tsx
@@ -4,32 +4,58 @@ import ChatList from '../screens/ChatList';
 import ChatScreen from '../screens/ChatScreen';
 import LoadingScreen from '../screens/LoadingScreen';
 import Colors from '../constants/Colors';
+import { OverlayProvider, Chat, useCreateChatClient } from 'stream-chat-expo';
 
 const Stack = createNativeStackNavigator();
 
+const chatApiKey = 'xm7bebbtpuaq';
+const chatUserId = 'testUser1';
+const chatUserName = 'testUser1';
+const chatUserToken =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidGVzdFVzZXIxIn0.GcklwXL-qVjnpkgQVPznV4TGh4taeVUPunf-xggdJzs';
+
+const user = {
+  id: chatUserId,
+  name: chatUserName,
+};
+
 export default function ChatNavigator() {
+  const chatClient = useCreateChatClient({
+    apiKey: chatApiKey,
+    userData: user,
+    tokenOrProvider: chatUserToken,
+  });
+
+  if (!chatClient) {
+    return <LoadingScreen />;
+  }
+
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerStyle: { backgroundColor: Colors.primary500 },
-        headerTintColor: Colors.secondary500,
-      }}
-    >
-      <Stack.Screen
-        name="Chats"
-        component={ChatList}
-        options={{
-          title: 'Chats',
-          headerTitleAlign: 'center',
-        }}
-      />
-      <Stack.Screen
-        name="ChatScreen"
-        component={ChatScreen}
-        options={{
-          headerTitle: 'Messages',
-        }}
-      />
-    </Stack.Navigator>
+    <OverlayProvider>
+      <Chat client={chatClient}>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: Colors.primary500 },
+            headerTintColor: Colors.secondary500,
+          }}
+        >
+          <Stack.Screen
+            name="Chats"
+            component={ChatList}
+            options={{
+              title: 'Chats',
+              headerTitleAlign: 'center',
+            }}
+          />
+          <Stack.Screen
+            name="ChatScreen"
+            component={ChatScreen}
+            options={{
+              headerTitle: 'Messages',
+            }}
+          />
+        </Stack.Navigator>
+      </Chat>
+    </OverlayProvider>
   );
 }
