28f280a1e9fc7ccc20c42f432561bd231e2a8057
Gabriel <gac232@cornell.edu>
2025-01-31 17:28:49 -0500

    update colors and shipwreck.

--- app1/constants/Colors.ts
+++ app1/constants/Colors.ts
@@ -1,11 +1,23 @@
 export default {
-  primary500: '#795ece', // maroon
-  primary200: '#d8cdf6', // off white
-  primary100: '#f2efff', // off white
+  primary500: '#1299a3', // main color
+  primary100: '#e8f6f7', // off white
   secondary500: '#bbbacf', // gray
   secondary200: '#f2f2f7', // light gray
   secondary100: '#FeFeFe', // almost white
-  green: '#85b6a0',
-  red: '#dd7994',
+  green: '#0b9c81bb',
+  red: '#c4485fbb',
   black: "#231f20",
 };
+
+
+// PURPLE COLOR SCHEME:
+// export default {
+//   primary500: '#795ece', // main color
+//   primary100: '#f2efff', // off white
+//   secondary500: '#bbbacf', // gray
+//   secondary200: '#f2f2f7', // light gray
+//   secondary100: '#FeFeFe', // almost white
+//   green: '#85b6a0',
+//   red: '#dd7994',
+//   black: "#231f20",
+// };
