8c2647579dffbcb8c07bd4465289bf11de3add9d
Gabriel Castillo <gac232@cornell.edu>
2025-05-03 17:14:49 -0400

    CHECKPOINT: before connecting images.

--- backshots/routes/routes.ts
+++ backshots/routes/routes.ts
@@ -1,60 +1,61 @@
-import express from 'express';
-import { Request, Response, NextFunction } from 'express';
-import { validateUser } from '../middleware/userValidation.js';
-import { validateSwipe } from '../middleware/swipeValidation.js';
+import express from "express";
+import { Request, Response, NextFunction } from "express";
+import { validateUser } from "../middleware/userValidation.js";
+import { validateSwipe } from "../middleware/swipeValidation.js";
 import {
   createSwipe,
   countRecentSwipes,
   getSwipesByUser,
-} from '../controllers/swipeController.js';
+} from "../controllers/swipeController.js";
 import {
   createUser,
   getAllUsers,
   getUserById,
   updateUser,
-} from '../controllers/userController.js';
+} from "../controllers/userController.js";
 
-import { getRecommendations } from '../controllers/algoDaddy.js';
+import { getRecommendations } from "../controllers/algoDaddy.js";
 import {
   generateUserToken,
   createChatChannel,
   updateChannelChatDisabled,
-} from '../controllers/chatController.js';
+} from "../controllers/chatController.js";
 import {
   uploadImage,
   getImage,
   deleteImage,
-} from '../controllers/imageController.js';
+} from "../controllers/imageController.js";
 
 const router = express.Router();
 
 // POST new user.
-router.post('/users', validateUser, createUser);
+router.post("/users", validateUser, createUser);
 
 // GET all users.
-router.get('/users', getAllUsers);
+router.get("/users", getAllUsers);
 
 // GET a specific user by ID
-router.get('/users/:id', getUserById);
+router.get("/users/:id", getUserById);
 
 // POST update a new user.
-router.post('/users/:id', updateUser);
+router.post("/users/:id", updateUser);
 
 // Swipe routes.
-router.post('/swipes', validateSwipe, createSwipe);
-router.get('/swipes/:userId/count', countRecentSwipes);
-router.get('/swipes/:userId', getSwipesByUser);
+router.post("/swipes", validateSwipe, createSwipe);
+router.get("/swipes/:userId/count", countRecentSwipes);
+router.get("/swipes/:userId", getSwipesByUser);
 
 // Get recommendations
-router.get('/users/:id/recommendations', getRecommendations);
+router.get("/users/:id/recommendations", getRecommendations);
 
-router.post('/chat/token', generateUserToken);
-router.post('/chat/channel', createChatChannel);
-router.post('/chat/channel/update', updateChannelChatDisabled);
+// Chat routes
+router.post("/chat/token", generateUserToken);
+router.post("/chat/channel", createChatChannel);
+router.post("/chat/channel/update", updateChannelChatDisabled);
 
 // Add image routes
-router.post('/images/upload', uploadImage);
-router.get('/images/:id', getImage);
-router.delete('/images/delete', deleteImage);
+router.post("/images/upload", uploadImage);
+router.get("/images/:id", getImage);
+router.delete("/images/delete", deleteImage);
 
 export default router;
