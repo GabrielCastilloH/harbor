041e455e827292c1e19cfae149352791d99f0d24
Gabriel Castillo <gac232@cornell.edu>
2025-05-18 15:20:48 -0400

    Add message count.

--- frontend/screens/ChatScreen.tsx
+++ frontend/screens/ChatScreen.tsx
@@ -1,11 +1,31 @@
 import { SafeAreaView, Text, View, StyleSheet } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import { Channel, MessageInput, MessageList } from "stream-chat-expo";
 import { useAppContext } from "../context/AppContext";
 import Colors from "../constants/Colors";
+import { updateMessageCount } from "../networking/ChatFunctions";
 
 export default function ChatScreen() {
   const { channel } = useAppContext();
+
+  useEffect(() => {
+    if (!channel) return;
+
+    // Set up message listener
+    const handleNewMessage = () => {
+      if (channel.id) {
+        updateMessageCount(channel.id).catch(console.error);
+      }
+    };
+
+    // Subscribe to new message events
+    channel.on("message.new", handleNewMessage);
+
+    // Cleanup listener on unmount
+    return () => {
+      channel.off("message.new", handleNewMessage);
+    };
+  }, [channel]);
 
   if (!channel) {
     return (
