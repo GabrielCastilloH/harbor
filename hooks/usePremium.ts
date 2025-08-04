a27d183f5795a133173b4c731c33507707076680
Gabriel Castillo <gac232@cornell.edu>
2025-08-04 16:44:33 +0700

    added swipe counter

---
+++ hooks/usePremium.ts
@@ -0,0 +1,22 @@
+import { useSuperwall } from "expo-superwall";
+import {
+  getFeatureConfig,
+  getSwipesPerDay,
+  canSeeWhoSwipedOnThem,
+  canSeeUnblurredImages,
+} from "../constants/Features";
+
+export const usePremium = () => {
+  const { isSubscribed } = useSuperwall();
+
+  // Use Superwall's isSubscribed as the source of truth
+  const isPremium = isSubscribed;
+
+  return {
+    isPremium,
+    features: getFeatureConfig(isPremium),
+    swipesPerDay: getSwipesPerDay(isPremium),
+    canSeeWhoSwipedOnThem: canSeeWhoSwipedOnThem(isPremium),
+    canSeeUnblurredImages: canSeeUnblurredImages(isPremium),
+  };
+};
