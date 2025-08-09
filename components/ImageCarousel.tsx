eda2646d93a181adfc447fbcb2895991d90d95b5
Gabriel Castillo <gac232@cornell.edu>
2025-08-09 23:43:02 +0700

    Refactor ImageCarousel styles by removing hardcoded background colors

--- components/ImageCarousel.tsx
+++ components/ImageCarousel.tsx
@@ -47,7 +47,6 @@ const ImageCarousel: React.FC<ImageCarouselProps> = ({
           justifyContent: "center",
           paddingHorizontal: spacing,
           height: imageSize + 60, // match stage height
-          backgroundColor: "yellow",
         }}
       >
         <View
@@ -57,7 +56,6 @@ const ImageCarousel: React.FC<ImageCarouselProps> = ({
               width: imageSize,
               height: imageSize,
               borderRadius,
-              backgroundColor: "orange",
             },
           ]}
         >
@@ -102,14 +100,13 @@ const ImageCarousel: React.FC<ImageCarouselProps> = ({
                 width: imageSize,
                 alignSelf: "center",
                 marginTop: 10,
-                backgroundColor: "purple",
               },
             ]}
           >
-            <Text style={[styles.clarityLabel, { backgroundColor: "cyan" }]}>
+            <Text style={[styles.clarityLabel]}>
               {Math.round(clarityPercent)}% Clear
             </Text>
-            <View style={[styles.clarityTrack, { backgroundColor: "brown" }]}>
+            <View style={[styles.clarityTrack]}>
               <View
                 style={[
                   styles.clarityFill,
@@ -163,14 +160,9 @@ const ImageCarousel: React.FC<ImageCarouselProps> = ({
   }, [images, currentIndex]);
 
   return (
-    <View style={{ width: "100%", backgroundColor: "red" }}>
+    <View style={{ width: "100%" }}>
       {/* Stage: image area with taps and indicators, fixed height */}
-      <View
-        style={[
-          styles.container,
-          { height: imageSize + 60, backgroundColor: "blue" },
-        ]}
-      >
+      <View style={[styles.container, { height: imageSize + 60 }]}>
         <FlatList
           ref={flatListRef}
           data={images}
@@ -204,12 +196,7 @@ const ImageCarousel: React.FC<ImageCarouselProps> = ({
               },
             ]}
           >
-            <View
-              style={[
-                styles.pageIndicator,
-                { width: imageSize, backgroundColor: "pink" },
-              ]}
-            >
+            <View style={[styles.pageIndicator, { width: imageSize }]}>
               {images.map((_, idx) => (
                 <View
                   key={idx}
