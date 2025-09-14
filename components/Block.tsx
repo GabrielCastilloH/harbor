501e49213ea3798542e3d328cb47a6ec44671468
Gabriel Castillo <gac232@cornell.edu>
2025-09-14 16:31:54 -0400

    basic blue red block implementation

---
+++ components/Block.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { StyleSheet, Dimensions } from "react-native";
+import {
+  PanGestureHandler,
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  useAnimatedGestureHandler,
+  withSpring,
+} from "react-native-reanimated";
+
+const { width: screenWidth } = Dimensions.get("window");
+const SWIPE_THRESHOLD = screenWidth * 0.2; // 20% of screen width
+
+const Post = () => {
+  const translateX = useSharedValue(0);
+
+  const gestureHandler = useAnimatedGestureHandler({
+    onStart: (_, context: any) => {
+      context.startX = translateX.value;
+    },
+    onActive: (event, context: any) => {
+      const nextTranslateX = context.startX + event.translationX;
+      // Clamp the value to prevent swiping too far off-screen
+      translateX.value = Math.max(
+        Math.min(nextTranslateX, 0), // Swiping starts from the left (red square), so max is 0
+        -screenWidth // Swipe left to the blue square
+      );
+    },
+    onEnd: (event) => {
+      if (event.translationX > SWIPE_THRESHOLD) {
+        // Swipe right: move to the red square
+        translateX.value = withSpring(0);
+      } else if (event.translationX < -SWIPE_THRESHOLD) {
+        // Swipe left: move to the blue square
+        translateX.value = withSpring(-screenWidth);
+      } else {
+        // Not enough of a swipe, snap back to the current position
+        translateX.value = withSpring(
+          translateX.value > -screenWidth / 2 ? 0 : -screenWidth
+        );
+      }
+    },
+  });
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: translateX.value }],
+    };
+  });
+
+  return (
+    <GestureHandlerRootView>
+      <PanGestureHandler
+        onGestureEvent={gestureHandler}
+        activeOffsetX={[-10, 10]}
+        activeOffsetY={[-20, 20]}
+      >
+        <Animated.View style={[styles.container, animatedStyle]}>
+          <Animated.View style={[styles.block, styles.redBlock]} />
+          <Animated.View style={[styles.block, styles.blueBlock]} />
+        </Animated.View>
+      </PanGestureHandler>
+    </GestureHandlerRootView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row", // Arrange children horizontally
+    width: screenWidth * 2, // Container needs to be twice the screen width
+    height: screenWidth,
+    marginVertical: 10,
+  },
+  block: {
+    width: screenWidth,
+    height: "100%",
+  },
+  redBlock: {
+    backgroundColor: "#FF0000",
+  },
+  blueBlock: {
+    backgroundColor: "#0000FF",
+  },
+});
+
+export default Post;
